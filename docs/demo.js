5dc923af70e87312a817e83c8ea1a6bd04f258c9
zhaixiang <843476168@qq.com>
2021-01-05 09:07:38 +0800

    feat:

--- docs/demo.js
+++ docs/demo.js
@@ -6,7 +6,7 @@ try {
 }
 
 // your account
-const uin = 123456789;
+const uin = 843476168
 const bot = createClient(uin, {
     log_level: "debug", //日志级别设置为debug
     platform: 1, //登录设备选择为手机
@@ -33,34 +33,34 @@ bot.on("system.online", ()=>{
 });
 
 //自动同意好友申请
-bot.on("request.friend.add", (data)=>{
-    bot.setFriendAddRequest(data.flag);
-});
+// bot.on("request.friend.add", (data)=>{
+//     bot.setFriendAddRequest(data.flag);
+// });
 
 //自动同意群邀请
-bot.on("request.group.invite", (data)=>{
-    bot.setGroupAddRequest(data.flag);
-});
+// bot.on("request.group.invite", (data)=>{
+//     bot.setGroupAddRequest(data.flag);
+// });
 
 //监听私聊
 bot.on("message.private", (data)=>{
-    // console.log(data);
-    bot.sendPrivateMsg(data.user_id, "hello");
+    console.log(data);
+    // bot.sendPrivateMsg(data.user_id, "hello");
 });
 
 //监听群聊
 bot.on("message.group", (data)=>{
-    // console.log(data);
-    bot.sendGroupMsg(data.group_id, "hello");
+    console.log(data);
+    // bot.sendGroupMsg(data.group_id, "hello");
 });
 
 //监听群员入群事件
-bot.on("notice.group.increase", (data)=>{
-    bot.sendGroupMsg(data.group_id, data.nickname + " 加入了群");
-});
+// bot.on("notice.group.increase", (data)=>{
+//     bot.sendGroupMsg(data.group_id, data.nickname + " 加入了群");
+// });
 
 // login with your password or password_md5
-bot.login("password"); 
+bot.login("zx2962Zy"); 
 
 //同一事件可以多次监听
 //更多api和事件请参考文档或client.d.ts文件
---
+++ docs/msg.js
@@ -0,0 +1,53 @@
+
+// [2021 - 01 - 03T15: 53: 48.717][INFO][BOT: 843476168]- recv from: [Private: 630723131(friend)]
+// [CQ: image, file = 29ec39d28c78c6aab5b0afd07fbfe72e21456 - 40 - 54.gif, url = http://c2cpicdw.qpic.cn/offpic_new/630723131//630723131-4140536087-29EC39D28C78C6AAB5B0AFD07FBFE72E/0?term=2]
+msg = {
+  self_id: 843476168,
+  time: 1609660428,
+  post_type: 'message',
+  message_type: 'private',
+  sub_type: 'friend',
+  message_id: 'JZgSOwAAnZ9W0S7zX/F4DA==',
+  user_id: 630723131,
+  message: [{ type: 'image', data: [Object] }],
+  raw_message: '[CQ:image,file=29ec39d28c78c6aab5b0afd07fbfe72e21456-40-54.gif,url=http://c2cpicdw.qpic.cn/offpic_new/630723131//630723131-4140536087-29EC39D28C78C6AAB5B0AFD07FBFE72E/0?term=2]',
+  font: '宋体',
+  sender: {
+    user_id: 630723131,
+    nickname: '當代梵高.',
+    sex: 'male',
+    age: 0,
+    area: 'unknown',
+    remark: '翟子墨'
+  },
+  auto_reply: false
+}
+
+// [2021-01-03T15:46:04.481] [INFO] 
+// [BOT:843476168] - recv from: [Group: 第十个名字书友群(789141078), Member: 疯狂的大耳朵兔子(2767071712)] 老迪你酒量如何
+msg = {
+  self_id: 843476168,
+  time: 1609659964,
+  post_type: 'message',
+  message_type: 'group',
+  sub_type: 'normal',
+  message_id: 'LwlWVqTuKeAAAQixOdnh+1/xdjwB',
+  group_id: 789141078,
+  group_name: '第十个名字书友群',
+  user_id: 2767071712,
+  anonymous: null,
+  message: [{ type: 'text', data: [Object] }],
+  raw_message: '老迪你酒量如何',
+  font: '宋体',
+  sender: {
+    user_id: 2767071712,
+    nickname: '刚好遇见你',
+    card: '疯狂的大耳朵兔子',
+    sex: 'male',
+    age: 15,
+    area: '广州',
+    level: 4,
+    role: 'member',
+    title: ''
+  }
+}
